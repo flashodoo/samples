1ca62db1743be9ebcea82fa9e1bd2e43ae92aeff
flashodoo <64464299+flashodoo@users.noreply.github.com>
2020-05-07 12:13:21 +0200

    Add files via upload

---
+++ ejemplo_test/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Test control",
+    "version": "12.0.1.2.0",
+    "category": "Test Form",
+    "license": "AGPL-3",
+    "summary": "A generic Test Form.",
+    "author": "Test team, "
+              "Community Team",
+    "website": "www.odoo.com",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/qc_menus.xml",
+        "views/qc_test_view.xml",
+    ],
+    "installable": True,
+}
